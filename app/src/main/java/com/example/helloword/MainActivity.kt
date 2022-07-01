866c46100104ca575e264b24bcec5a9db9ae7a2c
jcarlos <juancarlostf51@gmail.com>
2022-07-01 03:51:58 -0500

    Part one, navigation between screens

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.helloword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.helloword.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnShowName.setOnClickListener{ checkValue() }
+
+    }
+
+    fun checkValue (){
+        if(binding.etName.text.isNotEmpty()){
+
+        }else {
+            showErrorName();
+        }
+    }
+
+    private fun showErrorName() {
+        Toast.makeText(this, "El nombre de usuario no puede ser vacio!", Toast.LENGTH_SHORT).show()
     }
 }
